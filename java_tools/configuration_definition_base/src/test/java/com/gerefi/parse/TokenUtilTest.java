95344e9b0a1f88c75fac5758d3f64a0f0c87f421
Bruno Ferreira <126033267+gerefi@users.noreply.github.com>
2025-05-29 19:52:41 +0200

    upgrade java

---
+++ java_tools/configuration_definition_base/src/test/java/com/gerefi/parse/TokenUtilTest.java
@@ -0,0 +1,15 @@
+package com.gerefi.parse;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+import com.gerefi.parse.TokenUtil;
+
+public class TokenUtilTest {
+    @Test
+    public void test() {
+        assertEquals("first", TokenUtil.grabFirstTokenAndTheRest("first s").first);
+        assertEquals("second", TokenUtil.grabFirstTokenAndTheRest("f second \t").second);
+        assertEquals("second", TokenUtil.grabFirstTokenAndTheRest("f\tsecond\t ").second);
+    }
+}
