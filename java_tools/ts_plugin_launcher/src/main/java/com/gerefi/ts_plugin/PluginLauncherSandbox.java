b23a402f78cd919b8543fb55acd4f00ede4e594a
Bruno Ferreira <126033267+gerefi@users.noreply.github.com>
2025-05-29 21:34:59 +0200

    test

--- java_tools/ts_plugin_launcher/src/main/java/com/gerefi/ts_plugin/PluginLauncherSandbox.java
+++ java_tools/ts_plugin_launcher/src/main/java/com/gerefi/ts_plugin/PluginLauncherSandbox.java
@@ -8,7 +8,7 @@ import javax.swing.*;
 public class PluginLauncherSandbox {
     public static void main(String[] args) {
         JFrame frame = new JFrame();
-        frame.setSize(800, 500);
+        frame.setSize(500, 500);
         frame.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
 
         frame.add(new TsPluginLauncher().getPluginPanel());
